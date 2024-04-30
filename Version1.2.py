854d5f0ae9fb55e2ffb205e9b12ee5d8646f60a8
Serilii <cem.erden@web.de>
2024-04-30 13:49:39 +0200

    finished selling logic and some updates to "anzeige"

--- Version1.2.py
+++ Version1.2.py
@@ -609,20 +609,28 @@ def cauldron_list():        #needed for the open cauldron function, it's here fo
 #Status Bar
 def anzeige():
     os.system('cls')
+    x = len(p1.name) - len(current_enemy.name)  
+    x = -x if x < 0 else 0                          
+    y = len(current_enemy.name) - len(p1.name)
+    y = -y if y < 0 else 0                          #these 4 lines determine the distance needed to display Enemy name and Player name in on row
+    a = len(p1.name) - len("Position")  
+    a = -a if a < 0 else 0
+    b =  len("Position") - len(p1.name)
+    b = -b if b < 0 else 0
 
     print()
     if flag_fighting == True:
         print("////////////////////////////////////////////////////////////////////////////////////////")
-        print("|؏࿉࿈|\t" + p1.get_name() +"\t " + player_healthbar() + " HP: " + str(int(p1.hp)) + " / " + str(p1.maxhp) + "    Weapon: " + current_weapon.name + (("\tMana: " + str(p1.mana) + " / " + str(p1.maxmana)) if p1.mana > 0 else "" ))
-        print("|༖༗࿇|\t" + current_enemy.get_name()  + "\t" + enemy_healthbar() + " HP: " + str(current_enemy.hp) + " / " + str(current_enemy.maxhp))
+        print("|؏࿉࿈|\t" + p1.name + (x+4)* " " + player_healthbar() + " HP: " + str(int(p1.hp)) + " / " + str(p1.maxhp) + "    Weapon: " + current_weapon.name + (("\tMana: " + str(p1.mana) + " / " + str(p1.maxmana)) if p1.mana > 0 else "" ))
+        print("|༖༗࿇|\t" + current_enemy.name + (y+4)* " " + enemy_healthbar() + " HP: " + str(current_enemy.hp) + " / " + str(current_enemy.maxhp))
         print(r"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
         for i in buff_array:
             if i.counter > 0:
                 print(f"⬆ {i.name} for {i.counter} rounds" )
     else:
         print("________________________________________________________________________________________")
-        print("|؏࿉࿈|\t" + p1.get_name() + " HP: " + str(int(p1.hp)) + " / " + str(p1.maxhp) + "    Weapon: " + current_weapon.name + (("\tMana: " + str(p1.mana) + " / " + str(p1.maxmana)) if p1.mana > 0 else "" ) + "    Gold: " + str(p1.gold) )
-        print("|༖༗࿇|\tPosition  X(⇄) : " + str(p1.x) + "  Y(⇅): " + str(p1.y) + "\t\tArea: " + current_world[p1.x][p1.y].name)
+        print("|؏࿉࿈|\t" + p1.name + (a+4)*" " + "HP: " + str(int(p1.hp)) + " / " + str(p1.maxhp) + "    Weapon: " + current_weapon.name + (("\tMana: " + str(p1.mana) + " / " + str(p1.maxmana)) if p1.mana > 0 else "" ) + "    Gold: " + str(p1.gold) )
+        print("|༖༗࿇|\tPosition"+ (b+4)*" " + "X(⇄) : " + str(p1.x) + "  Y(⇅): " + str(p1.y) + "\t\tArea: " + current_world[p1.x][p1.y].name)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
  
 def player_healthbar():    #returns healthbar as string ♡♥
